5943dc01f647ee953a5da35f86ecc3b4bf193a6d
raudel Valdes <mac-pro@raudels-MacBook-Pro.local>
2019-02-13 20:38:02 -0500

    misc

--- index.js
+++ index.js
@@ -1,9 +1,49 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys.js');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'there Malik ;)' });
-});
+//making that bridge that will help us connect to googles auth
+//the callbackURL is the next endpoint that google will call
+//after being authenticated. After the callbackURL executes 
+//it will call a fucniton and console.log the access token
+passport.use(
+    new GoogleStrategy({
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback' 
+    }, (accessToken, refreshToken, profile, done) => {                    
+        console.log('accessToken: ',accessToken);
+        console.log('refresh token: ', refreshToken);
+        console.log('profile: ', profile);
+    })
+);
 
+ //asking google to allow the user to sign in and provide access 
+ //to the profile and email. scope specifies what user info we are
+ //trying to access. passport knows that the string 'google' is 
+ //refering to the the google strategy
+// app.get(
+//     '/auth/google',                   
+//     passport.authenticate('google', { 
+//         scope: ['profile', 'email'] 
+//     })
+// );
+
+app.get(
+    '/auth/google', () => {
+         "Valentines"
+    }
+);
+
+//this time around we are going to be recieving the user information
+//we will be sending in the token that the user was provided with when 
+//they accepted to give us premission to their account. Google will
+// verify the token and return the required information
+app.get('/auth/google/callback', passport.authenticate('google')); 
+
+//either do it in the port provided by huroku or on port 5000
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+
